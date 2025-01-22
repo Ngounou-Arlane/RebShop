42f679f5daa4922e20d5a7d4f241e9f05dc5d24d
Ngounou Arlane Otantik Nuna <authenticnuna123@gmail.com>
2025-01-22 16:35:37 +0100

    Update rebshop.sql

--- client/config/rebshop.sql
+++ client/config/rebshop.sql
@@ -1,23 +1,3 @@
--- --------------------------------------------------------
--- Hôte:                         localhost
--- Version du serveur:           8.0.30 - MySQL Community Server - GPL
--- SE du serveur:                Win64
--- HeidiSQL Version:             12.1.0.6537
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- Listage de la structure de la base pour rebshop
-CREATE DATABASE IF NOT EXISTS `rebshop` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `rebshop`;
 
 -- Listage de la structure de table rebshop. cart
 CREATE TABLE IF NOT EXISTS `cart` (
@@ -30,24 +10,30 @@ CREATE TABLE IF NOT EXISTS `cart` (
   CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Les données exportées n'étaient pas sélectionnées.
-
--- Listage de la structure de table rebshop. products
-CREATE TABLE IF NOT EXISTS `products` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `category` varchar(50) DEFAULT NULL,
-  `color` varchar(50) DEFAULT NULL,
-  `type` varchar(50) DEFAULT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `image` varchar(150) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Les données exportées n'étaient pas sélectionnées.
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+-- Listage des données de la table rebshop.cart : ~62 rows (environ)
+INSERT INTO `cart` (`id`, `user_id`, `product_id`, `quantity`) VALUES
+	(2, 2, 7, 1),
+	(3, 1, 10, 66),
+	(4, 3, 15, 1),
+	(5, 4, 30, 69),
+	(7, 2, 7, 1),
+	(8, 1, 10, 4),
+	(9, 3, 15, 1),
+	(10, 4, 30, 66),
+	(45, 0, 1, 1),
+	(47, 0, 2, 1),
+	(48, 0, 2, 1),
+	(49, 0, 1, 1),
+	(50, 0, 1, 1),
+	(51, 0, 2, 1),
+	(53, 0, 1, 1),
+	(54, 0, 2, 1),
+	(56, 42, 1, 1),
+	(57, 42, 2, 1),
+	(58, 42, 2, 1),
+	(60, 42, 1, 1),
+	(61, 42, 1, 1),
+	(65, 107, 8, 0),
+	(66, 107, 10, 3),
+	(67, 107, 11, 4),
+	(70, 107, 16, 1);
